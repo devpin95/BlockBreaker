a9619f19df333066bff5807afaedce2c15448699
Devin Piner <devpindroid@gmail.com>
2017-06-29 15:46:29 -0600

    Added menu options and functionality

--- js/levelClearScene.js
+++ js/levelClearScene.js
@@ -9,12 +9,22 @@ var level_info = {
 	score_bonus : 0
 };
 
+var retry_image = "assets/retry_button.png";
+var retry_image_hover = "assets/retry_button_hover.png";
+var next_image = "assets/next_button.png";
+var next_image_hover = "assets/next_button_hover.png";
+var RETRY;
+var NEXT;
+
 var bonuses_image;
 var bonuses_image_path = "assets/level_cleared.png";
 
 var levelClearScene = {
 	setup : function() {
 		bonuses_image = new block( 437, 139, bonuses_image_path, width/2 - 219, 25, 0, "image" );
+		RETRY = new block( 60, 30, retry_image, ( myGameArea.canvas.width / 2 ) - 30, 400, 0, "image" );
+		NEXT = new block( 143, 40, next_image, ( myGameArea.canvas.width / 2 ) - 71, 440, 0, "image" );
+
 		this.scene_ready = true;
 	},
 
@@ -50,6 +60,50 @@ var levelClearScene = {
 		// myGameArea.context.fillStyle = "#f00";
 		// myGameArea.context.textAlign = "center";
 		// myGameArea.context.fillText( "Bonuses", width/2, height/2 );
+
+		if (mousePos.x < RETRY.x + RETRY.width &&
+			mousePos.x > RETRY.x &&
+			mousePos.y < RETRY.y + RETRY.height &&
+			mousePos.y > RETRY.y ) 
+		{
+			//hovering
+			RETRY.image.src = retry_image_hover;
+			RETRY.height = 40;
+			RETRY.width = 80;
+			RETRY.x = ( myGameArea.canvas.width / 2 ) - 40;
+			RETRY.y = 395;
+
+		}
+		else
+		{
+			RETRY.image.src = retry_image;
+			RETRY.height = 30;
+			RETRY.width = 60;
+			RETRY.x = ( myGameArea.canvas.width / 2 ) - 30;
+			RETRY.y = 400;
+		}
+
+		if (mousePos.x < NEXT.x + NEXT.width &&
+			mousePos.x > NEXT.x &&
+			mousePos.y < NEXT.y + NEXT.height &&
+			mousePos.y > NEXT.y ) 
+		{
+			//hovering
+			NEXT.image.src = next_image_hover;
+			NEXT.height = 50;
+			NEXT.width = 179;
+			NEXT.x = ( myGameArea.canvas.width / 2 ) - 90;
+			NEXT.y = 435;
+
+		}
+		else
+		{
+			NEXT.image.src = next_image;
+			NEXT.height = 40;
+			NEXT.width = 143;
+			NEXT.x = ( myGameArea.canvas.width / 2 ) - 71;
+			NEXT.y = 440;
+		}
 
 		bonuses_image.update();
 
@@ -92,10 +146,32 @@ var levelClearScene = {
 			myGameArea.context.fillText( "Paddle Hits: +" + bonuses[3]( myPaddle.numberHits ), width/2, starting_y + (40 * next_line) );
 			++next_line;
 		}
+
+		RETRY.update();
+		NEXT.update();
 	},
 
 	clicked : function() {
-		++GAME_STATE.LEVEL;
+		//check if the mouse position is inside the RETRY button
+		if (mousePos.x < RETRY.x + RETRY.width &&
+			mousePos.x > RETRY.x &&
+			mousePos.y < RETRY.y + RETRY.height &&
+			mousePos.y > RETRY.y ) 
+		{
+			GAME_STATE.change_scene( SCENES.GAME_SCENE );
+		} 
+
+		else if (
+			mousePos.x < NEXT.x + NEXT.width &&
+			mousePos.x > NEXT.x &&
+			mousePos.y < NEXT.y + NEXT.height &&
+			mousePos.y > NEXT.y ) 
+		{
+			if ( GAME_STATE.LEVEL != GAME_STATE.HIGHEST_LEVEL ) {
+				++GAME_STATE.LEVEL;
+				GAME_STATE.change_scene( SCENES.GAME_SCENE );
+			}
+		}
 	},
 
 	button_press : function( e ) {
