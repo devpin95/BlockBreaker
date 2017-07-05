e6efd4441ad8536b0bfbdf5371dc0284bed13fd0
Devin Piner <devpindroid@gmail.com>
2017-07-05 14:11:58 -0600

    Added "retry" menu option

--- js/pausedScene.js
+++ js/pausedScene.js
@@ -1,14 +1,18 @@
 var paused_header_image = "assets/paused.png";
 var settings_image = "assets/settings_button.png";
 var settings_image_hover = "assets/settings_button_hover.png";
+var retry_image = "assets/retry_button.png";
+var retry_image_hover = "assets/retry_button_hover.png";
 
 var PAUSED_HEADER;
 var SETTINGS;
+var RETRY;
 
 var pausedScene = {
 	setup : function( ) {
 		PAUSED_HEADER = new block( 122, 50, paused_header_image, ( myGameArea.canvas.width / 2 ) - 61, 75, 0, "image" );
-		SETTINGS = new block( 90, 30, settings_image, ( myGameArea.canvas.width / 2 ) - 45, 150, 0, "image" );
+		RETRY = new block( 60, 30, retry_image, ( myGameArea.canvas.width / 2 ) - 30, 150, 0, "image" );
+		SETTINGS = new block( 90, 30, settings_image, ( myGameArea.canvas.width / 2 ) - 45, 190, 0, "image" );
 		this.scene_ready = true;
 	},
 
@@ -33,7 +37,7 @@ var pausedScene = {
 			SETTINGS.height = 40;
 			SETTINGS.width = 110;
 			SETTINGS.x = ( myGameArea.canvas.width / 2 ) - 55;
-			SETTINGS.y = 145;
+			SETTINGS.y = 185;
 
 		}
 		else
@@ -42,9 +46,32 @@ var pausedScene = {
 			SETTINGS.height = 30;
 			SETTINGS.width = 90;
 			SETTINGS.x = ( myGameArea.canvas.width / 2 ) - 45;
-			SETTINGS.y = 150;
+			SETTINGS.y = 190;
 		}
 		SETTINGS.update();
+
+		if (mousePos.x < RETRY.x + RETRY.width &&
+			mousePos.x > RETRY.x &&
+			mousePos.y < RETRY.y + RETRY.height &&
+			mousePos.y > RETRY.y ) 
+		{
+			//hovering
+			RETRY.image.src = retry_image_hover;
+			RETRY.height = 40;
+			RETRY.width = 80;
+			RETRY.x = ( myGameArea.canvas.width / 2 ) - 40;
+			RETRY.y = 145;
+
+		}
+		else
+		{
+			RETRY.image.src = retry_image;
+			RETRY.height = 30;
+			RETRY.width = 60;
+			RETRY.x = ( myGameArea.canvas.width / 2 ) - 30;
+			RETRY.y = 150;
+		}
+		RETRY.update();
 
 		myGameArea.context.font = "15px Arial";
 		myGameArea.context.fillStyle = "#000";
@@ -59,6 +86,13 @@ var pausedScene = {
 			mousePos.y > SETTINGS.y ) 
 		{
 			GAME_STATE.change_scene( SCENES.SETTINGS_SCENE );
+		} else if (
+			mousePos.x < RETRY.x + RETRY.width &&
+			mousePos.x > RETRY.x &&
+			mousePos.y < RETRY.y + RETRY.height &&
+			mousePos.y > RETRY.y ) 
+		{
+			 GAME_STATE.change_scene( SCENES.RETRY_LEVEL_PROMPT );
 		}
 	},
 
