7f035b97d163cb8ee886eb498abbd51542f1ec27
Devin Piner <devpindroid@gmail.com>
2017-07-09 14:50:59 -0600

    Added return to menu option

--- js/pausedScene.js
+++ js/pausedScene.js
@@ -3,16 +3,20 @@ var settings_image = "assets/settings_button.png";
 var settings_image_hover = "assets/settings_button_hover.png";
 var retry_image = "assets/retry_button.png";
 var retry_image_hover = "assets/retry_button_hover.png";
+var menu_image = "assets/menu_button.png";
+var menu_image_hover = "assets/menu_button_hover.png";
 
 var PAUSED_HEADER;
 var SETTINGS;
 var RETRY;
+var MENU;
 
 var pausedScene = {
 	setup : function( ) {
 		PAUSED_HEADER = new block( 122, 50, paused_header_image, ( myGameArea.canvas.width / 2 ) - 61, 75, 0, "image" );
 		RETRY = new block( 60, 30, retry_image, ( myGameArea.canvas.width / 2 ) - 30, 150, 0, "image" );
 		SETTINGS = new block( 90, 30, settings_image, ( myGameArea.canvas.width / 2 ) - 45, 190, 0, "image" );
+		MENU = new block( 60, 30, menu_image, ( myGameArea.canvas.width / 2 ) - 71, 480, 0, "image" );
 		this.scene_ready = true;
 	},
 
@@ -73,6 +77,29 @@ var pausedScene = {
 		}
 		RETRY.update();
 
+		if (mousePos.x < MENU.x + MENU.width &&
+			mousePos.x > MENU.x &&
+			mousePos.y < MENU.y + MENU.height &&
+			mousePos.y > MENU.y ) 
+		{
+			//hovering
+			MENU.image.src = menu_image_hover;
+			MENU.height = 40;
+			MENU.width = 80;
+			MENU.x = ( myGameArea.canvas.width / 2 ) - 40;
+			MENU.y = 225;
+
+		}
+		else
+		{
+			MENU.image.src = menu_image;
+			MENU.height = 30;
+			MENU.width = 60;
+			MENU.x = ( myGameArea.canvas.width / 2 ) - 30;
+			MENU.y = 230;
+		}
+		MENU.update();
+
 		myGameArea.context.font = "15px Arial";
 		myGameArea.context.fillStyle = "#000";
 		myGameArea.context.textAlign = "center";
@@ -93,6 +120,14 @@ var pausedScene = {
 			mousePos.y > RETRY.y ) 
 		{
 			 GAME_STATE.change_scene( SCENES.RETRY_LEVEL_PROMPT );
+		} else if (
+			mousePos.x < MENU.x + MENU.width &&
+			mousePos.x > MENU.x &&
+			mousePos.y < MENU.y + MENU.height &&
+			mousePos.y > MENU.y ) 
+		{
+			GAME_STATE.change_scene( SCENES.MAIN_MENU_SCENE );
+			SCENES.GAME_SCENE.reset_level();
 		}
 	},
 
