f45ef352078a57050f3ed4e16e9568402d18a4db
Devin Piner <devpindroid@gmail.com>
2017-07-19 00:45:48 -0600

    Erasing works again

--- js/lc_eraseBlocks.js
+++ js/lc_eraseBlocks.js
@@ -14,6 +14,14 @@ var eraseBlocksScene = {
 
 	click_down : function() {
 		mousePos.down = true;
+		for ( var i = test_blocks.length - 1; i >= 0; --i ) {
+			if ( mousePos.x >= test_blocks[i].left_edge && mousePos.x <= test_blocks[i].right_edge && mousePos.y >= test_blocks[i].top_edge && mousePos.y <= test_blocks[i].bottom_edge ){
+				test_blocks.splice( i, 1 );
+				level_object.blocks.splice( i, 1 );
+				mousePos.down = false;
+				break;
+			}
+		}
 	},
 
 	click_up : function() {
