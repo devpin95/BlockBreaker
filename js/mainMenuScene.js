91924579753a44364b0d5fd9d0afea163929d422
Devin Piner <devpindroid@gmail.com>
2017-07-27 19:29:25 -0600

    Moved main menu to new button system

--- js/mainMenuScene.js
+++ js/mainMenuScene.js
@@ -7,10 +7,21 @@ var PLAY, LOGO;
 
 var mainMenuScene = {
 	setup : function() {
-		bonuses_image = new block( 437, 139, bonuses_image_path, width/2 - 219, 25, 0, "image" );
-		PLAY = new block( 60, 30, play_image, ( myGameArea.canvas.width / 2 ) - 30, 100, 0, "image" );
+		//bonuses_image = new block( 437, 139, bonuses_image_path, width/2 - 219, 25, 0, "image" );
 		LOGO = new block( 260, 40, "assets/logo.png", ( myGameArea.canvas.width / 2 ) - 130, 75, 0, "image" );
-		SETTINGS = new block( 90, 30, settings_image, ( myGameArea.canvas.width / 2 ) - 45, 190, 0, "image" );
+		// PLAY = new block( 60, 30, play_image, ( myGameArea.canvas.width / 2 ) - 30, 100, 0, "image" );
+		// SETTINGS = new block( 90, 30, settings_image, ( myGameArea.canvas.width / 2 ) - 45, 190, 0, "image" );
+
+		this.menu = new menu();
+		//x1, x2, y1, y2, type, image, image_hover, callback
+		this.menu.add( ( myGameArea.canvas.width / 2 ) - 30, ( myGameArea.canvas.width / 2 ) - 35, 150, 145, "image", "assets/play_button.png", "assets/play_button_hover.png", function() {
+			this.menu_balls = [];
+			this.scene_ready = false;
+			GAME_STATE.change_scene( SCENES.GAME_SCENE );
+		} );
+		this.menu.add( ( myGameArea.canvas.width / 2 ) - 45, ( myGameArea.canvas.width / 2 ) - 55, 190, 185, "image", "assets/settings_button.png", "assets/settings_button_hover.png", function() {
+			GAME_STATE.change_scene( SCENES.SETTINGS_SCENE );
+		} );
 
 		this.menu_balls.push( new ball( 15, 15, default_ball_image, width / 2, height / 2, "image" ) );
 		var spd_x = (Math.random() * maxBallSpeed) + 1;
@@ -30,6 +41,7 @@ var mainMenuScene = {
 		this.scene_ready = true;
 	},
 
+	menu : null,
 	scene_ready : false,
 
 	menu_balls : [],
@@ -37,52 +49,8 @@ var mainMenuScene = {
 	run : function() {
 
 		LOGO.update();
-
-		if (mousePos.x < PLAY.x + PLAY.width &&
-			mousePos.x > PLAY.x &&
-			mousePos.y < PLAY.y + PLAY.height &&
-			mousePos.y > PLAY.y ) 
-		{
-			//hovering
-			PLAY.image.src = play_image_hover;
-			PLAY.height = 40;
-			PLAY.width = 70;
-			PLAY.x = ( myGameArea.canvas.width / 2 ) - 35;
-			PLAY.y = 145;
-
-		}
-		else
-		{
-			PLAY.image.src = play_image;
-			PLAY.height = 30;
-			PLAY.width = 60;
-			PLAY.x = ( myGameArea.canvas.width / 2 ) - 30;
-			PLAY.y = 150;
-		}
-		PLAY.update();
-
-		if (mousePos.x < SETTINGS.x + SETTINGS.width &&
-			mousePos.x > SETTINGS.x &&
-			mousePos.y < SETTINGS.y + SETTINGS.height &&
-			mousePos.y > SETTINGS.y ) 
-		{
-			//hovering
-			SETTINGS.image.src = settings_image_hover;
-			SETTINGS.height = 40;
-			SETTINGS.width = 110;
-			SETTINGS.x = ( myGameArea.canvas.width / 2 ) - 55;
-			SETTINGS.y = 185;
-
-		}
-		else
-		{
-			SETTINGS.image.src = settings_image;
-			SETTINGS.height = 30;
-			SETTINGS.width = 90;
-			SETTINGS.x = ( myGameArea.canvas.width / 2 ) - 45;
-			SETTINGS.y = 190;
-		}
-		SETTINGS.update();
+		this.menu.hovering( mousePos.x, mousePos.y );
+		this.menu.update();
 
 		for ( var i = 0; i < this.menu_balls.length; ++i ) {
 			myGameArea.collision( this.menu_balls[i] );
@@ -99,27 +67,8 @@ var mainMenuScene = {
 	},
 
 	clicked : function() {
-		//check if the mouse position is inside the RETRY button
-		if (mousePos.x < PLAY.x + PLAY.width &&
-			mousePos.x > PLAY.x &&
-			mousePos.y < PLAY.y + PLAY.height &&
-			mousePos.y > PLAY.y ) 
-		{
-			this.menu_balls = [];
-			this.scene_ready = false;
-			GAME_STATE.change_scene( SCENES.GAME_SCENE );
-		} 
 
-		else if (
-			mousePos.x < SETTINGS.x + SETTINGS.width &&
-			mousePos.x > SETTINGS.x &&
-			mousePos.y < SETTINGS.y + SETTINGS.height &&
-			mousePos.y > SETTINGS.y ) 
-		{
-			GAME_STATE.change_scene( SCENES.SETTINGS_SCENE );
-		}
-
-		else 
+		if ( !this.menu.click( mousePos.x, mousePos.y ) )
 		{
 			this.menu_balls.push( new ball( 15, 15, default_ball_image, mousePos.x, mousePos.y, "image" ) );
 
